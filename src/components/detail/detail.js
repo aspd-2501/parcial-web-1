d8a5a851735889361e6b5907415ded5b1107e5f2
Ana Sofía Padilla Daza <78044651+aspd-2501@users.noreply.github.com>
2024-09-27 00:06:17 -0500

    inicio del proyecto

---
+++ src/components/detail/detail.js
@@ -0,0 +1,5 @@
+import {IntlProvider} from 'react-intl';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
